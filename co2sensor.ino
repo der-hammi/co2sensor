16d21928e543084ff073e20ff8984a68971bbbf8
Jens Walter <jpw@cjwalter.de>
2020-11-09 21:10:41 +0100

    Zeilen entfernt

--- co2sensor.ino
+++ co2sensor.ino
@@ -1,6 +1,6 @@
 #include <Wire.h>    // I2C library
 #include <ccs811.h>
-#Test 
+ 
 /*
  * IIC address default 0x5A, the address becomes 0x5B if the ADDR_SEL is soldered.
  */
@@ -50,34 +50,5 @@ void loop() {
   }
   
   // Wait
-  delay(1000); 
-
-/*  int co2value; 
-  delay(1000);
-    if(sensor.checkDataReady() == true){
-        Serial.print("CO2: ");
-        co2value=sensor.getCO2PPM();
-        Serial.println(co2value);
-        Serial.print("ppm, TVOC: ");
-        Serial.print(sensor.getTVOCPPB());
-        Serial.println("ppb");
-        if (co2value > 500){
-            digitalWrite (ledPin, HIGH);
-            //Serial.print ("an");
-        }else{
-            digitalWrite (ledPin, LOW);
-            //Serial.print ("aus");
-        }
-    } else {
-        Serial.println("Data is not ready!");
-    }
-    /*!
-     * @brief Set baseline
-     * @param get from getBaseline.ino
-     */
-/*    sensor.writeBaseLine(0x847B);
-    //delay cannot be less than measurement cycle
-    //delay(1000);
-*/
-    
+  delay(1000);     
 }
